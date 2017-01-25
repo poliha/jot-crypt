e2fc3178974977b473771f96fc2fa79a5b76b327
Peter Oliha <poliha2002@yahoo.com>
2017-01-25 14:36:10 +0100

    initialised notes array

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,9 +7,16 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class Home {
-
+	public notes = [];
   constructor(public navCtrl: NavController) {
-    
+    this.notes = [{title: "note1", content: "note1 content"},
+    							{title: "note2", content: "note2 content"},
+    							{title: "note3", content: "note3 content"}
+    							];
   }
 
+  ionViewDidLoad(){
+  	console.log("home view loaded");
+  }
+  
 }
