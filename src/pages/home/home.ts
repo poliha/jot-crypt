257eeec40bda775a33f29d12c710b78a3c307be8
Peter Oliha <poliha2002@yahoo.com>
2017-01-26 16:38:21 +0100

    added new item button

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 import { NotePage } from '../note/note';
+import { Data } from '../../providers/data';
+
 
 @Component({
   selector: 'page-home',
@@ -9,21 +11,28 @@ import { NotePage } from '../note/note';
 })
 export class Home {
 	public notes = [];
-  constructor(public navCtrl: NavController) {
-    this.notes = [{title: "note1", content: "note1 content"},
-    							{title: "note2", content: "note2 content"},
-    							{title: "note3", content: "note3 content"}
-    							];
+  constructor(public navCtrl: NavController, public dataService: Data) {
+    // this.notes = [{title: "note1", content: "note1 content"},
+    // 							{title: "note2", content: "note2 content"},
+    // 							{title: "note3", content: "note3 content"}
+    // 							];
   }
 
   ionViewDidLoad(){
   	console.log("home view loaded");
+  	this.notes = this.dataService.getAll();
   }
 
   openNote(note){
   	console.log("note", note);
   	this.navCtrl.push(NotePage, {
     	note: note
+  	});
+  }
+
+  newNote(){
+  	this.navCtrl.push(NotePage, {
+    	note: {}
   	});
   }
 }
